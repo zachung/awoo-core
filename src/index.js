4976251f3ccf585d15ad3f5a8641366080484e0d
zachung <zach50931@gmail.com>
2020-07-30 15:55:54 +0800

    fix.remove symbols

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import Symbols from './data/Symbols'
 import Chunk from './lib/Chunk'
 import Item from './lib/Item'
 import Layer from './lib/Layer'
 import Stage from './lib/Stage'
 
 export {
-  Symbols,
   Chunk,
   Item,
   Layer,
--- test/module.test.js
+++ test/module.test.js
@@ -1,10 +1,7 @@
-const { Symbols, Chunk, Item, Layer, Stage } = require('../src/index')
+const { Chunk, Item, Layer, Stage } = require('../src/index')
 
 describe('測試模組載入', () => {
   it('各項載入', () => {
-    if (!Symbols) {
-      throw new Error('Symbols 載入失敗')
-    }
     if (!Chunk) {
       throw new Error('Chunk 載入失敗')
     }
@@ -16,14 +13,6 @@ describe('測試模組載入', () => {
     }
     if (!Stage) {
       throw new Error('Stage 載入失敗')
-    }
-  })
-  it('Symbols 內層載入測試', () => {
-    if (!Symbols.Blocks || Symbols.Blocks.length <= 0) {
-      throw new Error('Symbols 載入失敗')
-    }
-    if (!Symbols.Blocks[0]) {
-      throw new Error('Symbols index 0 not specific')
     }
   })
 })
