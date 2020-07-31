8aeaa5f928eb5bb36cc7fb02bac4cf5b5446d68a
zachung <zach50931@gmail.com>
2020-07-31 16:42:24 +0800

    feat.chunk reader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Chunk from './lib/Chunk'
+import ChunkReader from './lib/ChunkReader'
 import Item from './lib/Item'
 import Layer from './lib/Layer'
 import Stage from './lib/Stage'
 
 export {
   Chunk,
+  ChunkReader,
   Item,
   Layer,
   Stage
